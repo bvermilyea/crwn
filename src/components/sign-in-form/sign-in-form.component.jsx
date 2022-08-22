6dd364bc45e6fac759a50b06599b2833ae3d8f61
bvermilyea <69866361+bvermilyea@users.noreply.github.com>
2022-08-22 12:23:43 -0500

    Added the user context listener and cleaned up the sign in and sign up components

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 
+import FormInput from '../form-input/form-input.component'
+import Button from '../button/button.component'
+
 import {
   signInWithGooglePopup,
-  createUserDocumentFromAuth,
   signInAuthUserWithEmailAndPassword,
 } from '../../utils/firebase/firebase.utils'
 
-import FormInput from '../form-input/form-input.component'
-import Button from '../button/button.component'
 import './sign-in-form.styles.scss'
 
 const defaultFormFields = {
@@ -24,16 +24,14 @@ const SignInForm = () => {
   }
 
   const signInWithGoogle = async () => {
-    const { user } = await signInWithGooglePopup()
-    await createUserDocumentFromAuth(user)
+    await signInWithGooglePopup()
   }
 
   const handleSubmit = async (event) => {
     event.preventDefault()
 
     try {
-      const response = await signInAuthUserWithEmailAndPassword(email, password)
-      console.log(response)
+      await signInAuthUserWithEmailAndPassword(email, password)
       resetFormFields()
     } catch (error) {
       switch (error.code) {
