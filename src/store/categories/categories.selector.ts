f7b2aa2846cf3b23be24bb48d86de34e619fc23b
bvermilyea <69866361+bvermilyea@users.noreply.github.com>
2023-01-10 14:55:58 -0600

    Added TypeScript and Typed the categories

--- src/store/categories/categories.selector.ts
+++ src/store/categories/categories.selector.ts
@@ -1,6 +1,10 @@
 import { createSelector } from 'reselect'
 
-const selectCategoryReducer = (state) => state.categories
+import { CategoriesState } from './categories.reducer'
+
+import { CategoryMap } from './categories.types'
+
+const selectCategoryReducer = (state): CategoriesState => state.categories
 
 export const selectCategories = createSelector(
     [selectCategoryReducer],
@@ -8,12 +12,12 @@ export const selectCategories = createSelector(
 )
 export const selectCategoriesMap = createSelector(
     [selectCategories],
-    (categories) => 
+    (categories): CategoryMap => 
         categories.reduce((acc, category) => {
         const { title, items } = category
         acc[title.toLowerCase()] = items
         return acc
-        }, {})
+        }, {} as CategoryMap)
 )
 
 export const selectCategoriesIsLoading = createSelector(
